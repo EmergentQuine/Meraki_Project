8bd373f97d37df41b8bb7343a0c870f171e5fcf1
TheLastEmanator <110783259+TheLastEmanator@users.noreply.github.com>
2022-08-18 12:02:11 -0400

    Refactor

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,18 +1,20 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
 import * as api from '../api';
+
 
 export const getPosts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts();
-        dispatch({ type: 'FETCH_ALL', payload: data });
+        dispatch({ type: FETCH_ALL, payload: data });
     } catch(error) {
-        console.log(error.message);
+        console.log(error);
     }
 }
 
 export const createPost = (post) => async (dispatch) => {
     try {
         const { data } = await api.createPost(post);
-        dispatch({ type: 'CREATE', payload: data });
+        dispatch({ type: CREATE, payload: data });
     } catch (error) {
         console.log(error);
     }
@@ -21,16 +23,16 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
     try {
         const { data } = await api.updatePost(id, post);
-        dispatch({ type: 'UPDATE', payload: data });
+        dispatch({ type: UPDATE, payload: data });
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
     }
 }
 
 export const deletePost = (id) => async (dispatch) => {
     try {
         await api.deletePost(id);
-        dispatch({ type: 'DELETE', payload: id });
+        dispatch({ type: DELETE, payload: id });
     } catch (error) {
         console.log(error);
     }
@@ -39,7 +41,7 @@ export const deletePost = (id) => async (dispatch) => {
 export const likePost = (id) => async (dispatch) => {
     try {
         const { data } = await api.likePost(id);
-        dispatch({ type: 'UPDATE', payload: data });
+        dispatch({ type: UPDATE, payload: data });
     } catch (error) {
         console.log(error);
     }
