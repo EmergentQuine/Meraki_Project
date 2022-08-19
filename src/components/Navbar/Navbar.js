7c17d9635daf4f7bb788c81bb11ac6bb47f4e82c
TheLastEmanator <110783259+TheLastEmanator@users.noreply.github.com>
2022-08-19 16:12:50 -0400

    Add Google OAuth and manual auth logic

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useHistory, useLocation } from 'react-router-dom';
 import { AppBar, Avatar, Toolbar, Typography, Button } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
-
+import decode from 'jwt-decode';
 import useStyles from './styles';
 import meraki from '../../images/meraki.jpeg';
 
@@ -20,9 +20,12 @@ const Navbar = () => {
     };
 
     useEffect(() => {
-        const token = user?.token;
-        //JWT ... used for manual sign
-
+        const token = user?.token; 
+        if (token) {
+            const decodedToken = decode(token);
+            if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+        }
+        
         setUser(JSON.parse(localStorage.getItem('profile')));
     }, [location]); // eslint-disable-line react-hooks/exhaustive-deps
 
