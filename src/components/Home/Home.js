9faa687530cd6eba43aa3039451ce9e9487d7063
TheLastEmanator <110783259+TheLastEmanator@users.noreply.github.com>
2022-08-19 21:36:05 -0400

    Add search logic

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { Container, Grow, Grid, Paper, AppBar, TextField, Button } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import { useHistory, useLocation } from 'react-router-dom';
-import ChipInput from 'material-ui-chip-input'
+import ChipInput from 'material-ui-chip-input';
 
-import { getPosts } from '../../actions/posts';
+import { getPosts, getPostsBySearch } from '../../actions/posts';
 import Pagination from '../Pagination';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
@@ -24,10 +24,33 @@ const Home = () => {
     const page = query.get('page') || 1;
     const searchQuery = query.get('searchQuery');
     const classes = useStyles();
+    const [search, setSearch] = useState('');
+    const [tags, setTags] = useState([]);
 
     useEffect(() => {
         dispatch(getPosts());
     }, [currentId, dispatch]);
+
+
+    const searchPost = () => {
+        if (search.trim() || tags){
+            dispatch(getPostsBySearch({ search, tags: tags.join(',') }));
+        }
+        else{
+            history.push('/');
+        }
+    };
+
+
+    const handleKeyPress = (e) => {
+        if (e.keyCode === 13){
+            searchPost();
+        }
+    };
+
+    const handleAdd = (tag) => setTags([ ...tags, tag]);
+
+    const handleDelete = (tagToDelete) => setTags(tags.filter((tag) => tag !== tagToDelete));
 
     return (
         <Grow in>
@@ -42,10 +65,20 @@ const Home = () => {
                     name="search" 
                     variant="outlined"  
                     label="Search Meras"
+                    onKeyPress={handleKeyPress}
                     fullWidth
-                    value="TEST"
-                    onChange={() => {}}
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
                     />
+                    <ChipInput 
+                        style={{ margin: '10px 0' }}
+                        value={tags}
+                        onAdd={handleAdd}
+                        onDelete={handleDelete}
+                        label="Search Tags"
+                        variant="outlined"
+                    />
+                    <Button onClick={searchPost} className={classes.searchButton} variant="contained" color="primary">Search</Button>
                 </AppBar>
                     <Form currentId={currentId} setCurrentId={setCurrentId} />
                     <Paper elevation={6}>
