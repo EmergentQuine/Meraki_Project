441d59f96ccf237dfcafe9ee6a24e9a248e4f965
TheLastEmanator <110783259+TheLastEmanator@users.noreply.github.com>
2022-08-19 23:11:46 -0400

    Add loading state and clean up pagination UI

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,14 +1,14 @@
-import { FETCH_ALL, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
+import { START_LOADING, END_LOADING, FETCH_ALL, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
 import * as api from '../api';
 
 
 export const getPosts = (page) => async (dispatch) => {
     try {
+        dispatch ( { type: START_LOADING });
         const { data } = await api.fetchPosts(page);
 
-        
-
         dispatch({ type: FETCH_ALL, payload: data });
+        dispatch({ type: END_LOADING });
     } catch(error) {
         console.log(error);
     }
@@ -17,8 +17,10 @@ export const getPosts = (page) => async (dispatch) => {
 
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
     try {
+        dispatch ( { type: START_LOADING });
         const { data: { data } } = await api.fetchPostsBySearch(searchQuery);
         dispatch( { type: FETCH_BY_SEARCH, payload: data });
+        dispatch({ type: END_LOADING });
     } catch (error) {
         console.log(error);
     }
@@ -27,6 +29,7 @@ export const getPostsBySearch = (searchQuery) => async (dispatch) => {
 
 export const createPost = (post) => async (dispatch) => {
     try {
+        dispatch ( { type: START_LOADING });
         const { data } = await api.createPost(post);
         dispatch({ type: CREATE, payload: data });
     } catch (error) {
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -81,9 +81,11 @@ const Home = () => {
                     <Button onClick={searchPost} className={classes.searchButton} variant="contained" color="primary">Search</Button>
                 </AppBar>
                     <Form currentId={currentId} setCurrentId={setCurrentId} />
-                    <Paper elevation={6}>
-                        <Pagination />
-                    </Paper>
+                    {(!searchQuery && !tags.length) && (
+                        <Paper elevation={6} className={classes.pagination}>
+                            <Pagination />
+                        </Paper>
+                    )}
                 </Grid>
             </Grid>
         </Container>
